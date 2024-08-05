2aea74d0dbcf959bed1e539b0adb40c4f9a12d19
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-05 15:10:51 +0900

    refactor: Github-Actions, CodeDeploy, S3, ECR을 활용한 CI/CD 리팩토링

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 배포 시작 ---------------"
-cd /home/ubuntu/server
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop badang-server || true
+docker rm badang-server || true
+dock pull 058264467328.dkr.ecr.ap-northeast-2.amazonaws.com/badang-server:latest
+docker run -d --name badang-server -p 8080:8080 058264467328.dkr.ecr.ap-northeast-2.amazonaws.com/badang-server:latest
 echo "--------------- 배포 완료 ---------------"
