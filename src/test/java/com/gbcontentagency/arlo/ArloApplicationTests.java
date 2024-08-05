c3d0dce014d92c96a49477beebde1eaaa3200a63
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-05 12:32:40 +0900

    init: Initial commit

---
+++ src/test/java/com/gbcontentagency/arlo/ArloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbcontentagency.arlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
