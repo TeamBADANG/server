602b10a22116f99c08635572b076bd514b3bca40
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-08 20:03:01 +0900

    build: HTTPS 설정

--- src/main/java/com/gbcontentagency/arlo/apis/HelloController.java
+++ src/main/java/com/gbcontentagency/arlo/apis/HelloController.java
@@ -1,16 +1,15 @@
 package com.gbcontentagency.arlo.apis;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello, World! This is ARLO's outify");
+    @GetMapping("/healthcheck")
+    public String healthcheck() {
 
-        return "index";
+        return "Hi, there! This is Girin's world";
     }
+
 }
