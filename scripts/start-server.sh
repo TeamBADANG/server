4d5841507591e1bddc4dfadc2fbbbc2741870c46
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-08 21:21:50 +0900

    chore: 배포 sh파일 오타 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 배포 시작 ---------------"
 docker stop badang-server || true
 docker rm badang-server || true
-dock pull 058264467328.dkr.ecr.ap-northeast-2.amazonaws.com/badang-server:latest
+docker pull 058264467328.dkr.ecr.ap-northeast-2.amazonaws.com/badang-server:latest
 docker run -d --name badang-server -p 8080:8080 058264467328.dkr.ecr.ap-northeast-2.amazonaws.com/badang-server:latest
 echo "--------------- 배포 완료 ---------------"
