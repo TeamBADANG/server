16709642682fc105a1ab5e1911bb9e51ca075c1b
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-09 10:45:02 +0900

    chore: @Controller annotation 체크 후 롤백

--- src/main/java/com/gbcontentagency/arlo/apis/HelloController.java
+++ src/main/java/com/gbcontentagency/arlo/apis/HelloController.java
@@ -6,17 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 public class HelloController {
-
-    @GetMapping("/")
-    public String hello(Model model) {
-
-        model.addAttribute("message", "Hi, there! This is Girin's world!");
-
-        return "index";
-    }
 
     @GetMapping("/healthcheck")
     public String healthcheck() {
