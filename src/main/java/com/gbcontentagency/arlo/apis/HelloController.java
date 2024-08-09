83f12b274c8219b575a3254be21b6d0e39e01e05
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-09 10:41:13 +0900

    chore: helloController 수정

--- src/main/java/com/gbcontentagency/arlo/apis/HelloController.java
+++ src/main/java/com/gbcontentagency/arlo/apis/HelloController.java
@@ -1,15 +1,27 @@
 package com.gbcontentagency.arlo.apis;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@ResponseBody
 public class HelloController {
+
+    @GetMapping("/")
+    public String hello(Model model) {
+
+        model.addAttribute("message", "Hi, there! This is Girin's world!");
+
+        return "index";
+    }
 
     @GetMapping("/healthcheck")
     public String healthcheck() {
 
-        return "Hi, there! This is Girin's world";
+        return "Hi, there! This is Girin's world!";
     }
 
 }
