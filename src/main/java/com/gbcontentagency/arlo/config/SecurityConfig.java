60b6bbd45a0cb216319e30c34b16f7f9469135a0
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-12 00:37:41 +0900

    feat: Spring Security 단에서의 CORS 설정 및 JWT 관련 보안 설정

--- src/main/java/com/gbcontentagency/arlo/config/SecurityConfig.java
+++ src/main/java/com/gbcontentagency/arlo/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.gbcontentagency.arlo.config;
 
+import com.gbcontentagency.arlo.users.jwt.JwtFilter;
+import com.gbcontentagency.arlo.users.jwt.JwtUtil;
 import com.gbcontentagency.arlo.users.oauth2.CustomOAuth2UserService;
+import com.gbcontentagency.arlo.users.oauth2.CustomSuccessHandler;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,6 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -16,13 +25,39 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
-        this.customOAuth2UserService = customOAuth2UserService;
-    }
+    private final CustomSuccessHandler customSuccessHandler;
 
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler, JwtUtil jwtUtil) {
+        this.customOAuth2UserService = customOAuth2UserService;
+        this.customSuccessHandler = customSuccessHandler;
+        this.jwtUtil = jwtUtil;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+                        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                        corsConfiguration.setAllowCredentials(true);
+                        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                        corsConfiguration.setMaxAge(1800L);
+
+                        corsConfiguration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        corsConfiguration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return corsConfiguration;
+                    }
+                }));
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -30,10 +65,14 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable);
 
         http
+                .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        http
                 .oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService)
                         ))
+                        .successHandler(customSuccessHandler)
                 );
 
         http
