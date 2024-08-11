7396b2f2800416dab7e2c92d6309c3fd070fb41c
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-12 03:13:12 +0900

    chore: 불필요한 import문 제거

--- src/main/java/com/gbcontentagency/arlo/apis/UserController.java
+++ src/main/java/com/gbcontentagency/arlo/apis/UserController.java
@@ -1,6 +1,5 @@
 package com.gbcontentagency.arlo.apis;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
