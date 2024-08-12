c0d542336b621a5653087b3e7ae64b8c12f4528c
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-08-13 03:34:54 +0900

    chore: 백엔드 자체 테스트용 메인 및 프론트 연결용 메인 페이지 컨트롤러 분리

--- src/main/java/com/gbcontentagency/arlo/apis/HelloController.java
+++ src/main/java/com/gbcontentagency/arlo/apis/HelloController.java
@@ -8,7 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
+    public String hello(Model model) {
+
+        model.addAttribute("title", "Girin World");
+        model.addAttribute("message", "Hello, World! This is Girin's World!");
+
+        return "index";
+    }
+
+
+    @GetMapping("/main")
+    public String frontPage() {
 
         return "redirect:https://e6fe-118-176-146-86.ngrok-free.app";
     }
